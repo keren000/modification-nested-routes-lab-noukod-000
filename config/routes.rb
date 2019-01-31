5a729486ce936df73e44196c0c6c1d291c8e24c2
Keren-Happuc Fleurimond <keren@noukod.com>
2019-01-31 19:33:19 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :artists do
-    resources :songs, only: [:index, :show, :edit, :new]
+    resources :songs, only: [:index, :show, :new, :edit]
   end
   resources :songs
 end
